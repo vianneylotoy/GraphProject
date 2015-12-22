effe84f6b40be0af19e2d2ac2db2a0e4de25ddb1
Vianney Lotoy <vianneylotoy@gmail.com>
2015-12-23 00:56:35 +0100

    Create erreurs.h

---
+++ erreurs.h
@@ -0,0 +1,36 @@
+#ifndef ERREURS_H_
+#define ERREURS_H_
+
+typedef enum TypError{
+	
+
+
+	NonExistentGraph = -1,
+	
+	
+	
+	
+	NonExistentVertex= -2,
+	
+	
+	InvalideVertex = -3,
+	
+	
+	ExistentVertex = -4,
+	
+	
+	
+	ErrorFile = -5,
+	
+	
+	
+	ExistentRidge = -6,
+	
+	
+	NonExistentRidge = -7
+	
+	
+}TypError;
+
+
+#endif
